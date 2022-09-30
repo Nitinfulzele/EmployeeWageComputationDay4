4868a8b3de42ac888dbab818441574820423ecf1
Nitinfulzele <nitinfulzele99@gmail.com>
2022-10-01 01:42:16 +0530

    Calculate Wage Till Condition

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -26,6 +26,7 @@ public class EmployeeWageComputation {
 		// Employee Daily Wage
 
 		int dailyWage = 0;
+		int dailyWage2 = 0;
 
 		dailyWage = fullDayHour * wagePerHour;
 		System.out.println("Daily wage of Employee is " + dailyWage);
@@ -37,7 +38,7 @@ public class EmployeeWageComputation {
 
 		// Switch case
 
-		int dailyWage2 = partTimeHour * 10;
+		dailyWage2 = partTimeHour * 10;
 
 		empPresent = inp.nextInt(2);
 		switch (empPresent) {
@@ -52,18 +53,42 @@ public class EmployeeWageComputation {
 			break;
 
 		}
-		
+
 		// Monthly Wage Employee
 
-				int monthlyWage = 0;
-				int monthDays = 20;
+		int monthlyWage = 0;
+		int monthDays = 20;
 
-				monthlyWage = dailyWage * monthDays;
-				System.out.println("Monthly wage of Employee is " + monthlyWage);
+		monthlyWage = dailyWage * monthDays;
+		System.out.println("Monthly wage of Employee is " + monthlyWage);
 
-				monthlyWage = dailyWage2 * monthDays;
-				System.out.println("Monthly wage of part time Employee is " + monthlyWage);
+		monthlyWage = dailyWage2 * monthDays;
+		System.out.println("Monthly wage of part time Employee is " + monthlyWage);
 
+		// Calculate wage till condition reached for month
+
+		int isWorking;
+		int totalMonthlyWage2 = 0;
+		int workingDays = 0;
+		int workingHour = 0;
+
+		while (workingHour <= 100 && workingDays <= 20) {
+
+			int isworking = inp.nextInt(2);
+			workingDays = workingDays + 1;
+
+			switch (isworking) {
+			case 0:
+				totalMonthlyWage2 = totalMonthlyWage2 + wagePerHour * 4;
+				workingHour = workingHour + 4;
+				break;
+			case 1:
+				totalMonthlyWage2 = totalMonthlyWage2 + wagePerHour * 8;
+				workingHour = workingHour + 8;
+				break;
+			}
+		}
+		System.out.println("Employee Total monthly wage is " + totalMonthlyWage2);
 	}
 
 }
